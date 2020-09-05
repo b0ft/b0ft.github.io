22721b4112d07476d4683d9217011deff6e411f9
b0ft <aldysyach17@gmail.com>
2020-09-05 13:45:30 +0700

    Updates

---
+++ precache-manifest.9ffd94124a4dee826219d77f1ab994f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bca64100d88ac12d583f93f537dec44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5dcdaf4120609b18e7",
+    "url": "/static/css/main.1e016b47.chunk.css"
+  },
+  {
+    "revision": "aa1d307f08d6874b9540",
+    "url": "/static/js/2.012af9b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.012af9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5dcdaf4120609b18e7",
+    "url": "/static/js/main.a0994201.chunk.js"
+  },
+  {
+    "revision": "61cf5e91b46cfb928a6f",
+    "url": "/static/js/runtime-main.d53251fa.js"
+  }
+]);
