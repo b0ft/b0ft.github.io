22159c8281b4b63d7e7d85d446cc8da26cf99cca
b0ft <aldysyach17@gmail.com>
2020-09-10 07:55:02 +0700

    test google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './js/App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-177617748-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(
   <React.StrictMode>
