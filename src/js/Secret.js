e960f6d840e5ce6636c176dd3e7bb46af5f11d4c
Aldy Syachranie <45134306+b0ft@users.noreply.github.com>
2021-09-15 19:15:32 +0700

    secret value

---
+++ src/js/Secret.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/main.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
+
+function Secret() {
+  return (
+    Secret value is 6664792
+  );
+}
+
+export default About;
